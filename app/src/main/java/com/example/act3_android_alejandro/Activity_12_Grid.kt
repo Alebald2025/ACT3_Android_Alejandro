a0caffce0cacaae209a23764c6ec455181c24b96
Alejandro Baldeon E <a24josbalesp@inspedralbes.cat>
2026-03-12 20:39:46 +0100

    Activity grid añadido

---
+++ app/src/main/java/com/example/act3_android_alejandro/Activity_12_Grid.kt
@@ -0,0 +1,11 @@
+package com.example.act3_android_alejandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_12_Grid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity12_grid)
+    }
+}
---
+++ app/src/main/java/com/example/act3_android_alejandro/Activity_12_Linear.kt
@@ -0,0 +1,11 @@
+package com.example.act3_android_alejandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_12_Linear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity12_linear)
+    }
+}
