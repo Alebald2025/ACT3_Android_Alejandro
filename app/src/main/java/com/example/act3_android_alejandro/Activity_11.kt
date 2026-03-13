93345526f597ec215c55715fa62e477a859cc07c
Alejandro Baldeon E <a24josbalesp@inspedralbes.cat>
2026-03-13 20:25:47 +0100

    Activity 11 y menu main creado

---
+++ app/src/main/java/com/example/act3_android_alejandro/Activity_11.kt
@@ -0,0 +1,14 @@
+package com.example.act3_android_alejandro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_11 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_11)
+
+
+
+    }
+}
--- app/src/main/java/com/example/act3_android_alejandro/MainActivity.kt
+++ app/src/main/java/com/example/act3_android_alejandro/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.act3_android_alejandro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val activity_11 = findViewById<Button>(R.id.activity_11)
+        activity_11.setOnClickListener {
+            val intent = Intent(this, Activity_11::class.java)
+            startActivity(intent)
+        }
 
+        val activity_12 = findViewById<Button>(R.id.activity_12)
+        activity_12.setOnClickListener {
+            val intent = Intent(this, Activity_11::class.java)
+            startActivity(intent)
+        }
+
+        val activity_13 = findViewById<Button>(R.id.activity_13)
+        activity_13.setOnClickListener {
+            val intent = Intent(this, Activity_11::class.java)
+            startActivity(intent)
+        }
+
+        val activity_14 = findViewById<Button>(R.id.activity_14)
+        activity_14.setOnClickListener {
+            val intent = Intent(this, Activity_11::class.java)
+            startActivity(intent)
+        }
 
     }
 }
